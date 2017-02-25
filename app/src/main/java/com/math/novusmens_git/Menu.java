92b34508b7b219f588ad8875fe199814c993126a
mathilde <mathilde.grapin@gmail.com>
2017-02-25 19:02:08 +0100

    Initial commit

---
+++ app/src/main/java/com/math/novusmens_git/Menu.java
@@ -0,0 +1,13 @@
+package com.math.novusmens_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
